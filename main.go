9bcf7bd0b8dcaeb8268a96666d58902f1af05c1a
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-04-23 12:38:33 +0530

    add 4 basic compiler commands

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 // main() is the entry point to the riya compiler CLI.
@@ -10,5 +11,32 @@ func main() {
 	//cli args
 	args := os.Args[1:]
 
-	fmt.Println(args)
+	//riya compiler commands parsing
+	if len(args) == 0 { // about
+		fmt.Println(RIYA_VERSION)
+		fmt.Println("Riya is a programming language and compiler")
+		fmt.Println("For help:")
+		fmt.Println("  riya help")
+	} else if len(args) == 1 {
+		switch args[0] {
+		case "version": // version
+			fmt.Println(RIYA_VERSION)
+		case "help": // help
+			fmt.Println("Riya Compiler Commands:")
+			fmt.Println("1. riya                                -> print about information")
+			fmt.Println("2. riya version                        -> print compiler version")
+			fmt.Println("3. riya help                           -> print compiler commands")
+			fmt.Println("4. riya compile <filename.riya>        -> compile .riya file to executable")
+		}
+	} else if len(args) == 2 {
+		if args[0] == "compile" { // compile
+			filename := args[1]
+			if len(filename) > 5 && strings.HasSuffix(filename, ".riya") {
+				fmt.Printf("Compiling %s...\n", filename)
+			}
+		}
+	}
+
 }
+
+const RIYA_VERSION = "riya 0.1.0"
