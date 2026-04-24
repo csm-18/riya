c6a3670c87b4ca24284462489d3b03137f098e62
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-04-24 16:36:54 +0530

    add tokens debug print, some tokens to GetTokens()

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,5 +1,7 @@
 package compiler
 
+import "fmt"
+
 // for compiling a source file and all its dependencies
 func Compile(filename string) {
 	//read source file
@@ -8,5 +10,18 @@ func Compile(filename string) {
 		Filename: filename,
 		Content:  content,
 	}
+
+	//get tokens
+	tokens := GetTokens(srcFile)
+
+	//debug: print tokens
+	fmt.Println("=== TOKENS ===")
+	for i, token := range tokens {
+		fmt.Printf("[%d] Type: %-15s | Value: %-10s | Index: %d\n", i, token.Type, "'"+token.Value+"'", token.Index)
+	}
+	fmt.Println("==============")
+	//debug end
+
+	//parse tokens to AST
 
 }
--- compiler/lexer.go
+++ compiler/lexer.go
@@ -4,5 +4,30 @@ package compiler
 func GetTokens(srcFile SourceFile) []Token {
 	var tokens []Token
 
+	x := 0
+	for x < len(srcFile.Content) {
+		if srcFile.Content[x] == '(' {
+			token := Token{Type: TokenTypes["LeftParen"], Value: "(", Index: x}
+			tokens = append(tokens, token)
+			x += 1
+			continue
+		} else if srcFile.Content[x] == ')' {
+			token := Token{Type: TokenTypes["RightParen"], Value: ")", Index: x}
+			tokens = append(tokens, token)
+			x += 1
+			continue
+		} else if srcFile.Content[x] == '{' {
+			token := Token{Type: TokenTypes["LeftBrace"], Value: "{", Index: x}
+			tokens = append(tokens, token)
+			x += 1
+			continue
+		} else if srcFile.Content[x] == '}' {
+			token := Token{Type: TokenTypes["RightBrace"], Value: "}", Index: x}
+			tokens = append(tokens, token)
+			x += 1
+			continue
+		}
+		x += 1
+	}
 	return tokens
 }
