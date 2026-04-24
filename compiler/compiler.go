5934c618caeb18fc982e3754e93c382cb7570f90
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-04-24 05:36:54 +0530

    add ResolveImports() & CompileFile()

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,6 +1,18 @@
 package compiler
 
-func Compile(filename string) {
-	ResolveImports(filename)
+import "fmt"
 
+// for compiling a source file and all its dependencies
+func Compile(filename string) {
+	srcFiles := ResolveImports(filename)
+
+	if len(srcFiles) == 1 {
+		CompileFile(srcFiles[0])
+	}
+
+}
+
+// for compiling a single source file
+func CompileFile(srcFile SourceFile) {
+	fmt.Println("Compiling... ")
 }
--- compiler/resolve_imports.go
+++ compiler/resolve_imports.go
@@ -1,14 +1,12 @@
+// this file contains the logic for resolving imports in the source code.
 package compiler
 
-import "fmt"
-
-func ResolveImports(filename string) {
+func ResolveImports(filename string) []SourceFile {
 	content := ReadFileToString(filename)
 	srcFile := SourceFile{
 		Filename: filename,
 		Content:  content,
 	}
 
-	fmt.Println("resolving imports...")
-	fmt.Printf("%+#v \n", srcFile)
+	return []SourceFile{srcFile}
 }
