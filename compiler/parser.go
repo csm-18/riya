6ef4071bb4e2c527a6caa2413a45465ae018b247
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-04-25 12:52:49 +0530

    parser now parses main() function

--- compiler/parser.go
+++ compiler/parser.go
@@ -25,6 +25,40 @@ func ParseTokensToAST(tokens []Token, srcFile SourceFile) ASTNode {
 }
 
 func ParseFunctionNode(tokens []Token, x int, srcFile SourceFile) (ASTNode, int) {
-	newIndex := 0
-	return nil, newIndex
+	x += 1 // skip 'fun' keyword
+
+	var functionNode = FunctionNode{
+		Name:        "",
+		Parameters:  []FunctionParameterNode{},
+		ReturnTypes: []string{},
+		Body:        []ASTNode{},
+	}
+	if x < len(tokens) && tokens[x].Type != TokenTypes["Identifier"] {
+		PrintErrorWithPosition(srcFile, tokens[x].Index, "Expected function name after 'fun'")
+	} else {
+		functionNode.Name = tokens[x].Value
+		x += 1
+	}
+
+	//for now skip parameters and return types parsing, just check for '()' after function name
+	if tokens[x].Type != TokenTypes["LeftParen"] && x+1 < len(tokens) && tokens[x+1].Type != TokenTypes["RightParen"] {
+		PrintErrorWithPosition(srcFile, tokens[x].Index, "Expected '()' after function name")
+	} else {
+		x += 2
+	}
+
+	if tokens[x].Type != TokenTypes["LeftBrace"] {
+		PrintErrorWithPosition(srcFile, tokens[x].Index, "Expected '{' to start function body")
+	} else {
+		x += 1
+	}
+
+	//for now skip function body parsing, just check for '}' at the end of function body
+	if x < len(tokens) && tokens[x].Type != TokenTypes["RightBrace"] {
+		PrintErrorWithPosition(srcFile, tokens[x].Index, "Expected '}' to end function body")
+	} else {
+		x += 1
+	}
+
+	return functionNode, x
 }
