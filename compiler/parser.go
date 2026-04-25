a1247e5a8bb77744501eab238f42082b3af00176
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-04-25 11:07:27 +0530

    update ParseFunctionNode() signature with index

--- compiler/parser.go
+++ compiler/parser.go
@@ -2,17 +2,19 @@
 package compiler
 
 func ParseTokensToAST(tokens []Token, srcFile SourceFile) ASTNode {
-	var ast ASTNode
-
-	ast = FileNode{
+	var ast = FileNode{
 		Filename: srcFile.Filename,
-		Children: nil,
+		Children: []ASTNode{},
 	}
 
 	x := 0
 	for x < len(tokens) {
 		if tokens[x].Type == TokenTypes["Keyword"] && tokens[x].Value == "fun" {
 			//parse function definition
+			functionNode, newIndex := ParseFunctionNode(tokens[:], x, srcFile)
+			ast.Children = append(ast.Children, functionNode)
+			x = newIndex
+			continue
 		} else {
 			PrintErrorWithPosition(srcFile, tokens[x].Index, "Unexpected token: "+tokens[x].Value)
 		}
@@ -20,4 +22,9 @@ func ParseTokensToAST(tokens []Token, srcFile SourceFile) ASTNode {
 	}
 
 	return ast
+}
+
+func ParseFunctionNode(tokens []Token, x int, srcFile SourceFile) (ASTNode, int) {
+	newIndex := 0
+	return nil, newIndex
 }
