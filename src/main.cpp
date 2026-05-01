252624ac122cc6ca022b509a8aeea5a10609597c
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-05-01 13:14:56 +0530

    add getArgsFrom() function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+std::vector<std::string> getArgsFrom(int argc, char* argv[]);
+
+int main(int argc, char* argv[]) {
+    //cli args
+    std::vector<std::string> args = getArgsFrom(argc, argv);
+    for (const std::string &arg : args) {
+        std::cout << arg << std::endl;
+    }
     return 0;
+}
+
+// convert cli args into vector<string> without program name
+std::vector<std::string> getArgsFrom(int argc, char* argv[]) {
+    std::vector<std::string> args;
+    args.reserve(argc-1);
+    for (int i = 1; i < argc; ++i) {
+        args.emplace_back(argv[i]);
+    }
+    return args;
 }
