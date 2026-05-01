5da51c6db821eab04ab54e46da195170a4af216e
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-05-01 13:50:58 +0530

    cli: help command

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@ std::vector<std::string> getArgsFrom(int argc, char* argv[]);
 
 const std::string RIYA_VERSION = "riya 0.1.0\n";
 const std::string RIYA_ABOUT = "A high-level programming language\nFor help:\n riya help\n";
+const std::string RIYA_HELP = "riya commands:\n"
+        " 1. riya                -> print about info\n"
+        " 2. riya version        -> print riya version\n"
+        " 3. riya help           -> print riya commands\n";
 
 int main(int argc, char* argv[]) {
     //cli args
@@ -14,10 +18,12 @@ int main(int argc, char* argv[]) {
 
 
     if (args.empty()){
-        std::cout << RIYA_VERSION<< RIYA_ABOUT;
+        std::cout << RIYA_VERSION << RIYA_ABOUT;
     }else if (args.size() == 1){
         if (args[0] == "version"){
             std::cout << RIYA_VERSION;
+        }else if (args[0] == "help"){
+            std::cout << RIYA_HELP;
         }
     }
     return 0;
