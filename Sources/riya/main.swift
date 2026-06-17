c95166384b31fc9942a0cbe6fb643cf1cae08bf8
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-06-17 15:52:25 +0000

    cli: about command

--- Sources/riya/main.swift
+++ Sources/riya/main.swift
@@ -1,3 +1,14 @@
 import Foundation
 
-print("Hello World!")
+let RIYA_VERSION = "riya 0.1.0"
+
+let args = CommandLine.arguments[1...]
+
+if args.isEmpty {
+    print(RIYA_VERSION)
+    print("")
+    print("A simple programming language that compiles to JS.")
+    print("")
+    print("For help: ")
+    print("  riya help")        
+}
