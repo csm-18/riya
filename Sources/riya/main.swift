72193779e1ad9e941438ce08af1d79caafd8880f
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-06-17 16:33:15 +0000

    cli: compile file command

--- Sources/riya/main.swift
+++ Sources/riya/main.swift
@@ -12,19 +12,27 @@ if args.isEmpty {
     print("For help: ")
     print("  riya help")        
 }else if args.count == 1 {
-    switch args[0] {
-    case "version":
-        print(RIYA_VERSION)
-    case "help":
-        print("Riya commands:")
-        print("  riya                  ->  Print about message")
-        print("  riya <filename.riya>  ->  Compile .riya file to .js file")
-        print("  riya version          ->  Print riya version")
-        print("  riya help             ->  Print riya commands list")
-    default:
-        print("Error: Unknown command!")
-        exit(0)
-    }
+
+    if args[0].hasSuffix(".riya") && (args[0].count > 5){
+        print("compiling \(args[0])")
+        
+    }else{
+
+        switch args[0] {    
+            case "version":
+                print(RIYA_VERSION)
+            case "help":
+                print("Riya commands:")
+                print("  riya                  ->  Print about message")
+                print("  riya <filename.riya>  ->  Compile .riya file to .js file")
+                print("  riya version          ->  Print riya version")
+                print("  riya help             ->  Print riya commands list")
+            default:
+                print("Error: Unknown command!")
+                exit(0)
+        }
+    
+    }    
 }else{
     print("Error: Unknown command!")
     exit(0)
